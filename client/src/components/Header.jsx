453b6cbf7cbcb12426996b9322710b36de505b18
HAMMAD QAIMKHNAI <62787660+HammadQaimkhani@users.noreply.github.com>
2023-02-26 12:17:10 +0500

    Login Page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,22 +6,26 @@ const Header = () => {
     <>
       <header className='p-6 flex items-center justify-between font-roboto '>
         {/* logo div */}
-        <div className='flex items-center cursor-pointer '>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='w-10 h-10 -rotate-90 text-primary'>
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5'
-            />
-          </svg>
-          <h1 className='text-primary font-bold text-lg font-roboto'>airbhb</h1>
-        </div>
+        <Link to={"/"}>
+          <div className='flex items-center cursor-pointer '>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-10 h-10 -rotate-90 text-primary'>
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5'
+              />
+            </svg>
+            <h1 className='text-primary font-bold text-lg font-roboto'>
+              airbhb
+            </h1>
+          </div>
+        </Link>
 
         {/* middle section of the header */}
         <div className='flex  items-center shadow-md shadow-gray-300 py-2 px-4 rounded-full gap-2 border border-gray-300  '>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "./Header";
 
 const Login = () => {
   return (
     <>
       <Header />
-      <h1 className='mt-'>hello i am login page </h1>
+      <div className='mt-16 '>
+        <h1 className='text-4xl text-center'>Login</h1>
+        <form className='mt-8 flex flex-col gap-2 '>
+          <input
+            className='w-[30rem] border border-gray-500 rounded-md p-2 block mx-auto'
+            type='email'
+            placeholder='your@email.com'
+          />
+          <input
+            className='w-[30rem] border border-gray-500 rounded-md p-2 block mx-auto'
+            type='password'
+            placeholder='password'
+          />
+          <button className='border border-primary bg-primary text-white block mx-auto w-[30rem] rounded-md p-2 '>
+            Login
+          </button>
+        </form>
+        <p className='text-center mt-4'>
+          Don't have account yet? <Link to={"/register"}>Register now</Link>
+        </p>
+      </div>
     </>
   );
 };
