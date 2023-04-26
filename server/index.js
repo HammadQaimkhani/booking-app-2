23106f04f350f3a965b400fcdcd976697e01bb4f
HAMMAD QAIMKHNAI <62787660+HammadQaimkhani@users.noreply.github.com>
2023-04-26 18:00:24 +0500

    config

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ require("dotenv").config();
 const bycrptSlat = bycrpt.genSaltSync(10);
 
 // set JWT secert key
-const jwtSecert = "ajkakkajsfkasfksjk";
+const jwtSecert = process.env.JWT_SECERET;
 
 const app = express();
 
@@ -27,7 +27,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://127.0.0.1:5173",
+    origin: process.env.BASE_URL,
   })
 );
 app.use(cookieParser());
